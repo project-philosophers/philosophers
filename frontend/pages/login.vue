e75cd047c40fbc9263086cae70ee47e9f4441f15
Huang Cian Rong <angle007momo0s@gmail.com>
2022-02-03 02:07:33 +0900

    Login layout html and CSS

---
+++ frontend/pages/login.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<template>
+  <div class='login'>
+    <Nav />
+    <h1>login</h1>
+    <LoginForm />
+    <a href="https://colorlib.com/etc/lf/Login_v14/index.html">參考樣式</a>
+  </div>
+</template>
+
+<style>
+
+</style>
