9ec4ab22b40a319146073bc9c3d42c582a4bef90
Huang Cian Rong <angle007momo0s@gmail.com>
2022-02-15 23:48:06 +0900

    show page finished

--- frontend/components/PhilosopherShow.vue
+++ frontend/components/PhilosopherShow.vue
@@ -1,86 +1,45 @@
 <script>
 import { ref } from '@nuxtjs/composition-api'
-import { getName } from '../utils/philosopher.ts'
 
 export default {
   name: 'PhilosopherShow',
-  props:['philoId', 'philoInfo'],
+  props:['phId', 'phInfo'],
   setup(){
     const philosopher = ref({});
-    const influences = ref([]);
 
     return {
       philosopher,
-      next: 'edit',
     }
   },
   created() {
-    this.philosopher.value = this.philoInfo
+    this.philosopher.value = this.phInfo
   }
 }
 </script>
 
 <template>
-  <div class="h-screen flex flex-wrap justify-center p-10 bg-gray-100">
-    <div class='block w-full my-2'>
-      <span>id</span>
-      <span>{{philosopher.value.id}}</span>
-    </div>
-    <div class='block w-full my-2'>
-      <span>name:</span>
-      <span>{{philosopher.value.name}}</span>
-    </div>
-    <div class='block w-full my-2'>
-      <span>name_original:</span>
-      <span>{{philosopher.value.name_original}}</span>
-    </div>
-    <div class='block w-full my-2'>
-      <span>name_other:</span>
-      <span>{{philosopher.value.name_other}}</span>
-    </div>
-    <div class="block w-full my-2">
-      <span>Born:</span>
-      <span>{{ philosopher.value.born }}{{ philosopher.value.born_date ? ` ${philosopher.value.born_date }` :'' }}{{ philosopher.value.born_questioning? '(questioning)' :'' }}</span>
-    </div>
-    <div class="block w-full my-2">
-      <span>Born:</span>
-      <span>{{ philosopher.value.died }}{{ philosopher.value.died_date ? ` ${philosopher.value.died_date }` :''}}{{ philosopher.value.died_questioning? '(questioning)' :'' }}</span>
-    </div>
-    <div class='block w-full my-2'>
-      <span>gender:</span>
-      <span>{{philosopher.value.gender}}</span>
-    </div>
-    <div class='block w-full my-2'>
-      <span>birthplace:</span>
-      <span>{{philosopher.value.birthplace}}</span>
-    </div>
-    <div class='block w-full my-2'>
-      <span>deathplace:</span>
-      <span>{{philosopher.value.deathplace}}</span>
-    </div>
-    <div class='block w-full my-2'>
-      <span>influences:</span>
-      <span>{{philosopher.value.influences}}</span>
-    </div>
-    <div class='block w-full my-2'>
-      <span>influenced:</span>
-      <span>{{philosopher.value.influenced}}</span>
-    </div>
-    <div class='block w-full my-2'>
-      <span>createdAt:</span>
-      <span>{{philosopher.value.createdAt}}</span>
-    </div>
-    <div class='block w-full my-2'>
-      <span>updatedAt:</span>
-      <span>{{philosopher.value.updatedAt}}</span>
-    </div>
-
-
-  
-
+  <div class="h-screen p-10 bg-gray-100">
+    <template v-for="info in phInfo">
+      <template v-if="!!info.value">
+        <template v-if="info.label === 'Influenced' || info.label === 'Influences'">
+          <div :key="info.label"  class='block w-full my-1'>
+            <span>{{ info.label }}: </span>
+            <template v-for="person in info.value">
+              <span :key='person' class='inline-block bg-white px-2 py-1 m-1 rounded-lg'>{{ person }}</span>
+            </template>
+          </div>
+        </template>
+        <template v-else>
+          <div :key="info.label"  class='block w-full my-1'>
+            <span>{{ info.label }}: </span>
+            <span>{{ info.value }}</span>
+          </div>
+        </template>
+      </template>
+    </template>
     <button>
-      <!-- <nuxt-link :to="`/philosopher/${next}/${philoId}`">Edit</nuxt-link> -->
-      </button>
+      <nuxt-link :to="`/philosopher/edit/${phId}`">Edit</nuxt-link>
+    </button>
   </div>
 </template>
 
--- frontend/pages/philosopher/_mode/_id.vue
+++ frontend/pages/philosopher/_mode/_id.vue
@@ -8,20 +8,20 @@ import PhilosopherEdit from '../../../components/PhilosopherEdit.vue';
 import PhilosopherShow from '../../../components/PhilosopherShow.vue';
 
 // utils
-import { getPhilosophers } from '../../../utils/philosopher.ts';
+import { getPh, parsePh } from '../../../utils/philosopher.ts';
 
 export default {
   components: { PhilosopherShow, PhilosopherConfirm, PhilosopherEdit },
   name: 'philosopher',
   setup() {
       const route = useRoute()
-      const philoId = route.value.params.id;
+      const phId = route.value.params.id;
       const mode = route.value.params.mode;
 
     return {
-      philoId,
+      phId,
       mode,
-      data: getPhilosophers(philoId),
+      data: parsePh(getPh(phId)),
     }
   },
 }
@@ -31,13 +31,13 @@ export default {
   <div class='login'>
     <Nav />
     <template v-if="mode === 'show'">
-      <PhilosopherShow :philoInfo="data" :philoId="philoId"/>
+      <PhilosopherShow :phInfo="data" :phId="phId"/>
     </template>
     <template v-if="mode === 'edit'">
-      <PhilosopherEdit :data="data" :philoId="philoId"/>
+      <PhilosopherEdit :data="data" :phId="phId"/>
     </template>
     <template v-if="mode === 'confirm'">
-      <philosopher-Confirm :data="data" :philoId="philoId"/>
+      <philosopher-Confirm :data="data" :phId="phId"/>
     </template>
   </div>
 </template>
